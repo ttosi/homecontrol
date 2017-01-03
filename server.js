666f27f20623fe4f3a36379457f663424e330292
ttosi <ttosi519@gmail.com>
2017-01-03 12:56:38 -0800

    updating

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var Sugar = require('sugar');
 
 Sugar.extend();
 
-console.log("Running in :"  + process.env.NODE_ENV);
+console.log("Running in: "  + process.env.NODE_ENV);
 
 var times = SunCalc.getTimes(new Date(), 45.4, -122.8);
 console.log(times['sunrise'].format());
